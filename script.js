f421f8da79081a51c2278f23bc2bc766124b8210
noeger2001 <noevalladolid06@gmail.com>
2024-03-18 20:20:27 -0500

    version 1.1 se agrego css y condiciones

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const fs= requiere('fs')
+let cadena="noe"
+console.log("holaaa "+cadena)
+let edad= 19
+if(edad>=18){
+    console.log(cadena +"y a es mayor de edad")
+}else{
+    console.log(cadena+ " es menor de edad")
+}
